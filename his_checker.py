f7d8a44e8149286395417c83ff634f662ba78c9a
AfonsoSantos96 <afomms@gmail.com>
2023-08-10 23:23:19 +0100

    feat(HIS): add cyclomatic complexity metric check to script
    Signed-off-by: Afonso Santos <afomms@gmail.com>

--- his_checker.py
+++ his_checker.py
@@ -9,21 +9,45 @@ Generating HIS metrics check
 
 import sys
 import argparse
+import os
 
-METRIC_FAIL = 0
+CYCLOMATIC_TRESHOLD = 10
 
 def process_metrics(filename):
 
     """Process each HIS metric."""
-    print(filename)
+
+    metric_fail = 0
+    metric_fail += process_complexity(filename)
+    return metric_fail
+
+def process_complexity(file):
+
+    """Process McCabe Cyclomatic Complexity for each function in a file"""
+
+    metric_fail = 0
+    complexity = "pmccabe -c "
+    lines = os.popen(complexity + str(file))
+    lines = lines.read()
+    sline = lines.split('\n')
+
+    for num in range(len(sline)-1):
+        fields = sline[num].split('\t')
+        colony_count = int(fields[0])
+        if colony_count > CYCLOMATIC_TRESHOLD:
+            print("Complexity exceeded (max." + str(CYCLOMATIC_TRESHOLD) + "): "
+                  + fields[0] + " at " + fields[5])
+            metric_fail = 1
+    return metric_fail
 
 if __name__ == "__main__":
+    CHECK_FAIL = 0
     PARSER = argparse.ArgumentParser()
     PARSER.add_argument("files", nargs="+", help="The files to process")
     ARGS = PARSER.parse_args()
 
     for file_path in ARGS.files:
-        process_metrics(file_path)
+        CHECK_FAIL += process_metrics(file_path)
 
-    if METRIC_FAIL:
+    if CHECK_FAIL:
         sys.exit(-1)
